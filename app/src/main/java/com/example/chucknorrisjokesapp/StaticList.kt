4fd3161ae1fd976fd4b63e0d814bcf1ad3a291a0
mayisu7798 <maylis.montani@edu.esiee.fr>
2020-03-19 17:47:14 +0100

    Création d'une liste statiques de blagues

---
+++ app/src/main/java/com/example/chucknorrisjokesapp/StaticList.kt
@@ -0,0 +1,15 @@
+package com.example.chucknorrisjokesapp
+
+object StaticList {
+    val list = listOf( "Chuck Norris uses tabasco sauce instead of visine.",
+        "When you rub Chuck Norris' testicles, he will grant you three wishes. But remember, if you're a smartass and wish for more wishes, he will promptly skin you alive.",
+        "When Chuck Norris gets an erection, the street lights dim.",
+        "Chuck Norris once squeezed a lemon, got kiwi juice, put the kiwi juice the oven and it would be the best damn cupcake you ever had",
+        "Chuck Norris lives in a modest, one-room log cabin, built on the grounds of his Beverly Hills estate, next to the ordinance shed. His mansion is only for his hareem of bitches.",
+        "Date: March 8, 2012. Chuck Norris decides to spend his spring vacation in the Mushroom Kingdom to duke it out with the portly plumber himself. Mario is currently gobbling 1-Up mushrooms, grabbing red coins, and going berserk with golden flowers.",
+        "CHUCK NORRIS NEESDS TO USE A SEATBELT WHEN USING THE TOILET BECAUSE OF RECOIL!",
+        "Chuck Norris can taste lies.",
+        "Chuck Norris doesn't get morning wood, he gets morning Redwood!",
+        "Chuck Norris has a flying pony do you know how it flys,chuck hit it"
+    )
+}
