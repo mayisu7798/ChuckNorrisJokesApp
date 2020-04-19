08f68d3b7ab655bdcfbe86ee76090671b3967892
mayisu7798 <maylis.montani@edu.esiee.fr>
2020-04-19 11:43:48 +0200

    Gestion de la rotation et sauvegarde de l'état

--- app/src/main/java/com/example/chucknorrisjokesapp/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisjokesapp/MainActivity.kt
@@ -1,29 +1,34 @@
 package com.example.chucknorrisjokesapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View.*
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewTreeObserver
 import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.serialization.builtins.list
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.json.JsonConfiguration.Companion
 import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity()
+
+class MainActivity : AppCompatActivity ()
 {
     private val disposable = CompositeDisposable()
+    private val listeOfJokes: MutableList<Joke> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Log.d("liste", StaticList.list.toString())
         val recycler: RecyclerView = findViewById(R.id.RecyclerView)
         recycler.layoutManager = LinearLayoutManager(this)
-        val listeOfJokes: MutableList<Joke> = mutableListOf()
 
         val jokeService: JokeApiService = JokeApiServiceFactory.createJokeApiService()
         val progressBar: ProgressBar = findViewById(R.id.ProgressBar)
@@ -46,12 +51,23 @@ class MainActivity : AppCompatActivity()
         }
         recycler.adapter = adapteur
 
-        adapteur.onBottomReached(adapteur)
-        recycler.viewTreeObserver.addOnScrollChangedListener ( ViewTreeObserver.OnScrollChangedListener {
-                if (!recycler.canScrollVertically(1)) {
-                    adapteur.onBottomReached(adapteur)
-                }
+
+        if (savedInstanceState != null) {
+            val listeJokesString = savedInstanceState.getString("listeJokesString")
+            if (listeJokesString != null) {
+                val json = Json(JsonConfiguration.Default)
+                val listeJokesSaved = json.parse(Joke.serializer().list, listeJokesString)
+                listeJokesSaved.forEach{Joke -> adapteur.setJokes(Joke) }
             }
+        } else {
+            adapteur.onBottomReached(adapteur)
+        }
+
+        recycler.viewTreeObserver.addOnScrollChangedListener(ViewTreeObserver.OnScrollChangedListener {
+            if (!recycler.canScrollVertically(1)) {
+                adapteur.onBottomReached(adapteur)
+            }
+        }
         )
 
     }
@@ -59,6 +75,13 @@ class MainActivity : AppCompatActivity()
     override fun onDestroy() {
         super.onDestroy()
         disposable.clear()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        val json = Json(JsonConfiguration.Default)
+        val listeJokesString = json.stringify(Joke.serializer().list, listeOfJokes)
+        outState.putString("listeJokesString", listeJokesString)
+        super.onSaveInstanceState(outState)
     }
 
     private fun List<String>.toJokeList() : List<Joke> = map { stringJoke ->
